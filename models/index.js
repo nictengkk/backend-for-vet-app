ba1644f1b6fffd0572620fbb2bfcc678de87e50e
nictengkk <nictengkk@gmail.com>
2019-03-27 22:55:35 +0800

    create preconfigured sequelize instance to connect to db depending on env

---
+++ models/index.js
@@ -0,0 +1,36 @@
+const Sequelize = require("sequelize");
+//create the preconfigured sequelize instance
+const env = process.env.NODE_ENV || "development";
+const config = require(__dirname + "/../config/config.js")[env];
+let sequelize;
+//Connect to different database depending on env
+if (env === "production") {
+  sequelize = new Sequelize(config.url, config.options);
+} else {
+  sequelize = new Sequelize(
+    config.database,
+    config.username,
+    config.password,
+    config.options
+  );
+}
+
+//pass the models to the connection
+const models = {
+  Clinic: sequelize.import("./Clinic"),
+  Customer: sequelize.import("./Customer"),
+  Review: sequelize.import("./Review"),
+  Coordinate: sequelize.import("./Coordinate")
+};
+
+Object.keys(models).forEach(key => {
+  if ("associate" in models[key]) {
+    models[key].associate(models);
+  }
+});
+
+//Link up all models
+module.exports = {
+  sequelize,
+  ...models
+};
