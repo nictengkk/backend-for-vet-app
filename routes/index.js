dbb33ffba8c43f4c56d1fc9b8c6f4ae45531cf51
nictengkk <nictengkk@gmail.com>
2019-03-29 10:48:01 +0800

    update register and login routes include validation

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const { Customer } = require("../models/");
 const bcrypt = require("bcrypt");
 const router = express.Router();
 
@@ -9,13 +10,33 @@ router.route("/register").get((req, res) => {
 router.route("/register").post(async (req, res) => {
   try {
     const customer = await Customer.create(req.body);
-    //check that existing user dont exist
     //create a page view that says your account has been successfully created
-    return res.status(204).redirect("/");
+    return res.status(201).json(customer);
+    // .redirect("/");
     //how to link back to "/"
   } catch (error) {
     console.error(error.message);
     return res.status(400).json({ error: error.message });
+  }
+});
+
+router.route("/login").post(async (req, res) => {
+  try {
+    const { email, password } = req.body;
+    const customer = await Customer.findOne({ where: { email } });
+    if (!customer) {
+      throw new Error("Please register for an account!");
+    }
+
+    const match = await bcrypt.compare(password, customer.password);
+
+    if (!match) {
+      throw new Error("Your password is invalid, please try again.");
+    }
+    return res.status(200).json({ success: "You are successfully logged in" });
+    // const {id} = customer
+  } catch (error) {
+    return res.status(401).send(error.message);
   }
 });
 
