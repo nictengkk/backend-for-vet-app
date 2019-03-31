b26595d4a5865b9c0896dcefe6d11e1edb8dbbed
nictengkk <nictengkk@gmail.com>
2019-03-31 10:39:59 +0800

    install cors and cookieParser library, update whitelist and corsOptions

--- app.js
+++ app.js
@@ -1,17 +1,43 @@
 const express = require("express");
-// const cors = require("cors");
+const cors = require("cors");
 const app = express();
 
 const clinics = require("./routes/clinics");
 const index = require("./routes/index");
+const users = require("./routes/users");
+const cookieParser = require("cookie-parser");
+
+const isDev = process.env.NODE_ENV !== "production";
+
+const whitelist = [
+  "https://findynearestvet.netlify.com",
+  "https://backend-for-vet-app.herokuapp.com/"
+];
+
+if (isDev) {
+  whitelist.push("http://localhost:3000");
+}
+
+const corsOptions = {
+  origin(origin, callback) {
+    if (whitelist.indexOf(origin) !== -1 || !origin) {
+      callback(null, true);
+    } else {
+      callback(new Error("Not allowed by CORS"));
+    }
+  },
+  credentials: true
+};
 
 // middleware
-// app.use(cors());
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cookieParser());
 
 // routes
 app.use("/", index);
 app.use("/api/clinics", clinics);
+app.use("/api/users", users);
 
 module.exports = app;
